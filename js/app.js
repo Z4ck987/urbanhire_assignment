d96ccc08d1c2f272a985ab0cd017b982829ada85
Z4ck987 <syamsierfan@gmail.com>
2017-04-23 06:10:47 +0700

    Add app.js

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var app = angular.module('API', []);
+
+app.controller('WeatherController', function($http, $scope) {
+	$http.get('http://api.openweathermap.org/data/2.5/weather?q=Jakarta&units=metric&mode=json&APPID=f9585deb2d84ed107c7f0c72f30819ac').
+	then(function(resp) {
+		$scope.Data = resp.data;
+		$scope.Data.city = resp.data.name;
+		$scope.Data.country = resp.data.sys.country;
+		$scope.Data.temp = resp.data.main.temp;
+		$scope.Data.deg = 'C';
+		$scope.Data.change = false;
+		$scope.Data.desc = resp.data.weather[0].main;
+
+	$scope.Data.sys = function() {
+		if ($scope.Data.change) {
+			$scope.Data.deg = 'C';
+			$scope.Data.temp = Math.round(resp.data.main.temp);
+			return $scope.Data.change = false;
+		} else {
+			$scope.Data.deg = 'F';
+			$scope.Data.temp = Math.round((resp.data.main.temp * 9)/5 + 32);
+			return $scope.Data.change = true;
+		}
+	};
+
+	});
+});
+
